d22af0055ed79f5692a53fd1dbd93847153a3a0a
Alvin Amaya <amayapinedaalvinasael@gmail.com>
2026-03-01 23:43:24 -0600

    Add controllers

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,58 @@
+﻿using Books_Api.Entities;
+using Books_Api.Features;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Books_Api.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksAppService _booksAppService;
+
+        public BooksController(BooksAppService booksAppService)
+        {
+            this._booksAppService = booksAppService;
+        }
+
+        // GET api/<BooksController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Book> books = _booksAppService.GetBooks();
+            return Ok(books);
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            return Ok(_booksAppService.GetBookById(id));
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Book book)
+        {
+            _booksAppService.AddBook(book);
+            return Ok("Book added successfully");
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromRoute] int id, [FromBody] Book book)
+        {
+            _booksAppService.UpdateBook(id, book);
+            return Ok("Book updated successfully");
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _booksAppService.DeleteBook(id);
+            return Ok("Book deleted successfully");
+        }
+    }
+}
