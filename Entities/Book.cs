191d8da56c797ab0087a574ef71b3f0a7e8e038c
Alvin Amaya <amayapinedaalvinasael@gmail.com>
2026-03-01 23:43:02 -0600

    Add entities

---
+++ Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Books_Api.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+    }
+}
